49ede1446cb252bb8d29d2a9dce989a51a623374
choiipad <choiipad@users.noreply.github.com>
2019-04-13 20:34:01 +0900

    Update cache.js

--- search/cache.js
+++ search/cache.js
@@ -1,1 +1,7 @@
 javascript:(function(){location.href="http://www.google.com/search?q=cache:"+escape(location.href.replace(/^http%5C:%5C/%5C/(.*)$/,"$1"))})()
+
+javascript:location.href='http://webcache.googleusercontent.com/search?q=cache:'+window.location.href
+
+javascript:location.href='http://www.google.com/search?q=cache:'+Qr.replace(/http:\/\//,'')
+
+javascript:void((function(){var a=location.href.replace(/^http\:\/\/(.*)$/,"$1");location.href="http://www.google.com/search?q=cache:" escape(a);})()) 
